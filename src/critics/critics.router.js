1741eaf5dc262431153892e4f83079c9d1637540
William Pratt <76258183+WPratt0211@users.noreply.github.com>
2021-05-21 13:08:01 -0500

    Delete src directory

--- src/critics/critics.router.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("./critics.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
-
-router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:critic_id").get(controller.read).all(methodNotAllowed);
-
-module.exports = router;
